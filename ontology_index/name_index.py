48a853e1fef01939d270e533585a7c388615d9bd
Tim Rozday <48996806+timrozday@users.noreply.github.com>
2021-03-02 09:57:39 +0000

    Update name_index.py

--- ontology_index/name_index.py
+++ ontology_index/name_index.py
@@ -169,3 +169,234 @@ class NameIndex():
         except:
             pass
         
+class QualifierIndex():
+    """For extraction of allowed qualifiers from indications
+
+From `NCIT` and `HPO`.
+
+`NCIT`:
+* `http://purl.obolibrary.org/obo/NCIT_C41009` Qualifier
+  * `http://purl.obolibrary.org/obo/NCIT_C13442` Anatomical qualifier
+  * `http://purl.obolibrary.org/obo/NCIT_C21514` Temporal qualifier
+  * `http://purl.obolibrary.org/obo/NCIT_C73706` Spatial qualifier
+  * `http://purl.obolibrary.org/obo/NCIT_C27992` Disease qualifier
+    * `http://purl.obolibrary.org/obo/NCIT_C28102` Disease clinical qualifier
+  * `http://purl.obolibrary.org/obo/NCIT_C27993` General qualifier (contains 'primary')
+  
+`HPO`:
+* `http://purl.obolibrary.org/obo/HP_0031797` Clinical course
+  * `http://purl.obolibrary.org/obo/HP_0011008` Temporal pattern
+  * `http://purl.obolibrary.org/obo/HP_0003679` Pace of progression
+  * `http://purl.obolibrary.org/obo/HP_0003674` Onset
+* `http://purl.obolibrary.org/obo/HP_0012823` Clinical modifier
+  * `http://purl.obolibrary.org/obo/HP_0025280` Pain characteristic
+  * `http://purl.obolibrary.org/obo/HP_0012824` Severity
+* `http://purl.obolibrary.org/obo/HP_0040279` Frequency
+
+Miscellaneous
+* `http://purl.obolibrary.org/obo/NCIT_C159950` Relapse
+* `http://purl.obolibrary.org/obo/NCIT_C43623` Persistent
+* `http://purl.obolibrary.org/obo/NCIT_C25251` Primary
+* `http://purl.obolibrary.org/obo/NCIT_C14174` Metastatic
+* `http://purl.obolibrary.org/obo/NCIT_C25378` Partial
+* `http://purl.obolibrary.org/obo/NCIT_C28242` Idiopathic
+* `http://purl.obolibrary.org/obo/NCIT_C34340` Accidental
+  """
+    
+    def __init__(self, data_dir='.'):
+        self.data_dir = data_dir
+        
+        try:
+            self.load_indexes()
+        except:
+            pass
+
+    def get_iri_terms(self, q):
+        iri, source = q
+        iri_str = urllib.parse.quote(iri, safe='')
+        iri_str = urllib.parse.quote(iri_str, safe='')
+        r = requests.get(f"https://www.ebi.ac.uk/ols/api/ontologies/{source}/terms/{iri_str}", params={'size':10, 'page':1})
+        return r.json()
+
+    def get_iri_decendents(self, q):
+        iri, source = q
+        iri_str = urllib.parse.quote(iri, safe='')
+        iri_str = urllib.parse.quote(iri_str, safe='')
+        data = []
+        p = 0
+        while True:
+            r = requests.get(f"https://www.ebi.ac.uk/ols/api/ontologies/{source}/terms/{iri_str}/hierarchicalDescendants", params={'size': 50, 'page': p}).json()
+            if '_embedded' in r:
+                data.extend(r['_embedded']['terms'])
+            if r['page']['number'] >= r['page']['totalPages'] -1:
+                break
+            else:
+                p+=1
+        return data
+
+    def normalise_whitespace(self, s):
+        s = re.sub('\s+', ' ', s)
+        s = re.sub('\s+$', '', s)
+        s = re.sub('^\s+', '', s)
+        return s
+
+    def filter_name(self, s):
+
+        exclude_suffixes = {
+            ', nos',
+            ',nos',
+            ', not otherwise specified',
+            ', unspecified',
+            ', not elsewhere classified'
+        }
+
+        s = s.lower()
+        if re.match('.*(\s|^)\(.*\)(\s|$).*', s):
+            s = re.sub('(\s|^)\(.*\)(\s|$)', ' ', s)
+
+            # tidy up problems that occur due to removing brackets
+            s = self.normalise_whitespace(s)
+
+            if bool(s):
+                if s[-1] == ',':
+                    s = self.normalise_whitespace(s[:-1])
+                if s[0] == ',':
+                    s = self.normalise_whitespace(s[1:])
+
+        else:
+            s = self.normalise_whitespace(s)
+
+        if re.match('.*(\s|^)\[.*\](\s|$).*', s):
+            s = re.sub('(\s|^)\[.*\](\s|$)', ' ', s)
+
+            # tidy up problems that occur due to removing brackets
+            s = self.normalise_whitespace(s)
+
+            if bool(s):
+                if s[-1] == ',':
+                    s = self.normalise_whitespace(s[:-1])
+                if s[0] == ',':
+                    s = self.normalise_whitespace(s[1:])
+
+        else:
+            s = self.normalise_whitespace(s)
+
+        s = re.sub('(\s|^)&(\s|$)', 'and', s)
+        s = re.sub('[/\-]', ' ', s)
+        s = re.sub('[^a-z., ]', '', s)
+        s = re.sub('(\s|^)\.\s', ' ', s)
+
+        for suffix in exclude_suffixes:
+            if s[-len(suffix):] == suffix:
+                s = self.normalise_whitespace(s[:-len(suffix)])
+
+        return s
+
+    def tokenize(self, s):
+        return [self.normalise_whitespace(t) for t in re.split('(?<=\S)[\s](?=\S)', s)]
+    
+    def gen_indexes(self, ncit=True, hpo=True, miscellaneous=True):
+
+        hp_qualifiers = {}
+        if hpo:
+            for iri in {'http://purl.obolibrary.org/obo/HP_0031797',
+                        'http://purl.obolibrary.org/obo/HP_0011008', 
+                        'http://purl.obolibrary.org/obo/HP_0003679', 
+                        'http://purl.obolibrary.org/obo/HP_0003674', 
+                        'http://purl.obolibrary.org/obo/HP_0012823',
+                        'http://purl.obolibrary.org/obo/HP_0025280', 
+                        'http://purl.obolibrary.org/obo/HP_0012824', 
+                        'http://purl.obolibrary.org/obo/HP_0040279'}:
+                for r in self.get_iri_decendents((iri, 'hp')):
+                    hp_qualifiers[(r['iri'], r['ontology_name'])] = {r['label']}
+                    if 'synonyms' in r:
+                        if r['synonyms']:
+                            hp_qualifiers[(r['iri'], r['ontology_name'])].update(r['synonyms'])
+
+        ncit_qualifiers = {}
+        if ncit:
+            for iri in {
+            #     'http://purl.obolibrary.org/obo/NCIT_C41009', # Qualifier
+                'http://purl.obolibrary.org/obo/NCIT_C13442', # Anatomical qualifier
+                'http://purl.obolibrary.org/obo/NCIT_C21514', # Temporal qualifier
+            #     'http://purl.obolibrary.org/obo/NCIT_C73706', # Spatial qualifier
+                'http://purl.obolibrary.org/obo/NCIT_C27992', # Disease qualifier
+                'http://purl.obolibrary.org/obo/NCIT_C28102', # Disease clinical qualifier
+                'http://purl.obolibrary.org/obo/NCIT_C27993', # General qualifier (contains 'primary')
+            }:
+                for r in self.get_iri_decendents((iri, 'ncit')):
+                    ncit_qualifiers[(r['iri'], r['ontology_name'])] = {r['label']}
+                    if 'synonyms' in r:
+                        if r['synonyms']:
+                            ncit_qualifiers[(r['iri'], r['ontology_name'])].update(r['synonyms'])
+
+        if miscellaneous:
+            custom_qualifiers = {
+                ('http://purl.obolibrary.org/obo/NCIT_C159950', 'ncit'): {'relapse', 'relapsed'},
+                ('http://purl.obolibrary.org/obo/NCIT_C43623', 'ncit'): {'persistent'},
+                ('http://purl.obolibrary.org/obo/NCIT_C25251', 'ncit'): {'primary'},
+                ('http://purl.obolibrary.org/obo/NCIT_C14174', 'ncit'): {'metastatic'},
+                ('http://purl.obolibrary.org/obo/NCIT_C25378', 'ncit'): {'partial'},
+                ('http://purl.obolibrary.org/obo/NCIT_C28242', 'ncit'): {'idiopathic'},
+                ('http://purl.obolibrary.org/obo/NCIT_C34340', 'ncit'): {'accidental'},
+            }
+        else:
+            custom_qualifiers = {}
+
+        self.ols_qualifiers = {**hp_qualifiers, **custom_qualifiers, **ncit_qualifiers}
+
+        token_qualifier_index = defaultdict(set)
+        for (iri, source), terms in self.ols_qualifiers.items():
+            for s in terms:
+                for t in self.tokenize(self.filter_name(s)):
+                    token_qualifier_index[t].add((s, (iri, source)))
+
+        self.token_qualifier_index = dict(token_qualifier_index)
+
+        
+    def save_indexes(self, data_dir=None):
+        if data_dir is None:
+            data_dir = self.data_dir
+            
+        with open(f'{data_dir}/ols_token_qualifier_index.pkl', 'wb') as f:
+            pickle.dump(self.token_qualifier_index, f)
+        with open(f'{data_dir}/ols_qualifiers.pkl', 'wb') as f:
+            pickle.dump(self.ols_qualifiers, f)
+            
+    def load_indexes(self, data_dir=None):
+        if data_dir is None:
+            data_dir = self.data_dir
+            
+        with open(f'{data_dir}/ols_token_qualifier_index.pkl', 'rb') as f:
+            self.token_qualifier_index = pickle.load(f)
+        with open(f'{data_dir}/ols_qualifiers.pkl', 'rb') as f:
+            self.ols_qualifiers = pickle.load(f)
+
+            
+    def extract_qualifiers(self, q):
+        q_tokens = set(self.tokenize(self.filter_name(q)))
+        candidate_matches = set()
+        for t in q_tokens:
+            if t in token_qualifier_index:
+                candidate_matches.update(token_qualifier_index[t])
+
+    #     return candidate_matches
+        candidate_matches_2 = set()
+        for m,(iri,source) in candidate_matches:
+            m_tokens = set(self.tokenize(self.filter_name(m)))
+            if not m_tokens - q_tokens:
+                candidate_matches_2.add((m,(iri,source)))
+
+        filter_q = re.sub('-', ' ', q.lower())
+        qualifiers = set()
+        for m,(iri,source) in sorted(candidate_matches, key=lambda x:(len(x[0]),x[0],x), reverse=True):
+            matches = list(re.finditer(self.filter_name(m), filter_q))
+            if matches:
+                qualifiers.add((m,(iri,source)))
+                match_spans = [match.span() for match in matches]
+                for (start, end) in sorted(match_spans, reverse=True):
+                    q = self.normalise_whitespace(q[:start] + q[end:])
+                    filter_q = re.sub('-', ' ', q.lower())
+
+
+        return q, tuple(qualifiers)
